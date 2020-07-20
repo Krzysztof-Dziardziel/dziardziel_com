82ee8958e0b659cd552d58320dbd858e7d123698
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-07-20 14:47:10 +0200

    Finished homeBanner design and modified to SelectableText widget

--- lib/homeBanner/bannerInfo.dart
+++ lib/homeBanner/bannerInfo.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../constants.dart';
 
@@ -7,62 +8,91 @@ class BannerInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Howdy!",
-                  style: GoogleFonts.montserrat(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 70,
-                      color: darkGray),
-                ),
-                Row(
-                  children: [
-                    Container(
-                      width: 0.01 * width,
-                      child: Expanded(
-                        child: Divider(
-                          color: gray,
-                          thickness: 3,
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+        width * 0.03,
+        0,
+        0,
+        0,
+      ),
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SelectableText(
+                    "Howdy!",
+                    style: GoogleFonts.montserrat(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 70,
+                        color: darkGray),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                        child: Container(
+                          width: 0.02 * width,
+                          child: Expanded(
+                            child: Divider(
+                              color: gray,
+                              thickness: 2,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    Text(
-                      "INTRODUCTION",
-                      style: GoogleFonts.roboto(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 20,
-                          color: gray),
-                    ),
-                  ],
-                ),
-              ],
+                      SelectableText(
+                        "INTRODUCTION",
+                        style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                            color: gray),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          Container(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "JS & Flutter Developer\nYear 13 A-Level Student",
-                  style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w400, fontSize: 25, color: gray),
-                ),
-                Text(
-                  "ALWAYS EAGER TO TRY A NEW CHALLENGE;\nJUST LET ME KNOW!",
-                  style: GoogleFonts.roboto(
-                      fontWeight: FontWeight.w200, fontSize: 18, color: gray),
-                ),
-              ],
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SelectableText(
+                    "JS & Flutter Developer\nYear 13 A-Level Student",
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400, fontSize: 25, color: gray),
+                  ),
+                  SelectableText(
+                    "ALWAYS EAGER TO TRY A NEW CHALLENGE;\nJUST LET ME KNOW!",
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w300, fontSize: 20, color: gray),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
+            FlatButton(
+              color: yellow,
+              onPressed: () => launch('mailto:krzysztof@dziardziel.com'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(5.0),
+                child: Text(
+                  'CONTACT ME',
+                  style: GoogleFonts.roboto(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
