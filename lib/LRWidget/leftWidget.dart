0ac34826b77358e9c74537214b46a31e8edabf6c
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-07-21 18:47:39 +0200

    Redesigned leftWidget

--- lib/LRWidget/leftWidget.dart
+++ lib/LRWidget/leftWidget.dart
@@ -16,121 +16,113 @@ class LeftWidget extends StatelessWidget {
     return Container(
       width: width,
       height: 0.4 * height,
-      child: Row(
+      child: Stack(
         children: [
           Container(
-            width: width * 0.48,
-            color: Colors.white,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(
-                10,
-                10,
-                0,
-                10,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            color: red,
+          ),
+          ClipPath(
+            child: Container(
+              color: Colors.white,
+            ),
+            clipper: LeftClip(),
+          ),
+          Row(
+            children: [
+              Container(
+                width: 0.49 * width,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 10, 0, 10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
-                      Padding(
-                        padding: EdgeInsets.all(15),
-                        child: Image(
-                          height: 0.07 * height,
-                          image: AssetImage(logo),
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Image(
+                              height: 0.07 * height,
+                              image: AssetImage(logo),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: Container(
+                              child: Wrap(
+                                spacing: 15,
+                                children: [
+                                  if (stack.contains('flutter'))
+                                    Image(
+                                      height: 0.07 * height,
+                                      image: AssetImage(
+                                        'assets/technologies/flutter@2x.png',
+                                      ),
+                                    ),
+                                  if (stack.contains('js'))
+                                    Image(
+                                      height: 0.07 * height,
+                                      image: AssetImage(
+                                        'assets/technologies/js@2x.png',
+                                      ),
+                                    ),
+                                  if (stack.contains('nodejs'))
+                                    Image(
+                                      height: 0.07 * height,
+                                      image: AssetImage(
+                                        'assets/technologies/nodejs@2x.png',
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
                       ),
                       Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 20),
-                        child: Container(
-                          child: Wrap(
-                            spacing: 15,
-                            children: [
-                              if (stack.contains('flutter'))
-                                Image(
-                                  height: 0.07 * height,
-                                  image: AssetImage(
-                                    'assets/technologies/flutter@2x.png',
-                                  ),
-                                ),
-                              if (stack.contains('js'))
-                                Image(
-                                  height: 0.07 * height,
-                                  image: AssetImage(
-                                    'assets/technologies/js@2x.png',
-                                  ),
-                                ),
-                              if (stack.contains('nodejs'))
-                                Image(
-                                  height: 0.07 * height,
-                                  image: AssetImage(
-                                    'assets/technologies/nodejs@2x.png',
-                                  ),
-                                ),
-                            ],
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: SelectableText(
+                          mainText,
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w300,
+                            fontSize: 20,
+                            color: gray,
                           ),
                         ),
-                      )
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("a"),
+                            Text('B'),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8),
-                    child: SelectableText(
-                      mainText,
-                      style: GoogleFonts.roboto(
-                        fontWeight: FontWeight.w300,
-                        fontSize: 20,
-                        color: gray,
+                ),
+              ),
+              Container(
+                width: 0.17 * width,
+              ),
+              Container(
+                width: 0.34 * width,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: red,
+                      image: DecorationImage(
+                        image: AssetImage(img),
                       ),
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("a"),
-                        Text('B'),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            width: width * 0.17,
-            child: Stack(
-              children: [
-                Container(
-                  color: Colors.white,
-                ),
-                ClipPath(
-                  child: Container(
-                    width: width,
-                    color: red,
-                  ),
-                  clipper: LeftClip(),
-                )
-              ],
-            ),
-          ),
-          Container(
-            width: width * 0.35,
-            color: red,
-            child: Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: red,
-                  image: DecorationImage(
-                    image: AssetImage(img),
-                  ),
                 ),
               ),
-            ),
-          ),
+            ],
+          )
         ],
       ),
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -26,9 +26,9 @@ class LeftClip extends CustomClipper<Path> {
   Path getClip(size) {
     Path path = Path();
     path.lineTo(0, 0);
-    path.lineTo(size.width, 0);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, 0);
+    path.lineTo(0, size.height);
+    path.lineTo(0.66 * size.width, size.height);
+    path.lineTo(0.49 * size.width, 0);
     return path;
   }
 
