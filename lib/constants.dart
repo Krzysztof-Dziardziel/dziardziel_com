f9e5b015cfb1ab79de043e32c9b1a04d2cf8c525
Krzysztof Dziardziel <krzysztof.dziardziel@students.akademeia.edu.pl>
2020-07-22 22:44:32 +0200

    End of branch

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,10 +10,10 @@ class HomeBannerClip extends CustomClipper<Path> {
   @override
   Path getClip(size) {
     Path path = Path();
-    path.lineTo(0, 0);
-    path.lineTo(0, size.height);
+    path.lineTo(0.52 * size.width, 0);
+    path.lineTo(0.34 * size.width, size.height);
+    path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
     return path;
   }
 
@@ -44,7 +44,6 @@ class RightClip extends CustomClipper<Path> {
     path.lineTo(0.44 * size.width, size.height);
     path.lineTo(size.width, size.height);
     path.lineTo(size.width, 0);
-
     return path;
   }
 
